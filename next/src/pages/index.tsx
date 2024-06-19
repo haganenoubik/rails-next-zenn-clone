4dd2b365eab8d0d9f2144ad8f47c08429cb21f9b
haganenoubik <haganenoubik@gmail.com>
2024-06-19 22:52:52 +0900

    記事一覧ページのモックを実装

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,24 +1,49 @@
-import type { NextPage } from 'next'
-import useSWR from 'swr'
-import { fetcher } from '@/utils'
+import { Box, Grid, Container } from "@mui/material";
+import camelcaseKeys from "camelcase-keys";
+import type { NextPage } from "next";
+import Link from "next/link";
+import useSWR from "swr";
+import ArticleCard from "@/components/ArticleCard";
+import { fetcher } from "@/utils";
 
-type HealthCheckResponse = {
-  message: string
-}
+type ArticleProps = {
+  id: number;
+  title: string;
+  createdAt: string;
+  fromToday: string;
+  user: {
+    name: string;
+  };
+};
 
 const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
-  const { data, error } = useSWR<HealthCheckResponse>(url, fetcher)
+  const url = "http://localhost:3000/api/v1/articles";
 
-  if (error) return <div>エラーが発生しました</div>
-  if (!data) return <div>Loading...</div>
+  const { data, error } = useSWR(url, fetcher);
+  if (error) return <div>An error has occurred.</div>;
+  if (!data) return <div>Loading...</div>;
+
+  const articles = camelcaseKeys(data.articles);
 
   return (
-    <>
-      <div>Rails疎通チェック</div>
-      <div>レスポンスメッセージ： {data.message}</div>
-    </>
-  )
-}
+    <Box sx={{ backgroundColor: "#e6f2ff", minHeight: "100vh" }}>
+      <Container maxWidth="md" sx={{ pt: 6 }}>
+        <Grid container spacing={4}>
+          {articles.map((article: ArticleProps, i: number) => (
+            <Grid key={i} item xs={12} md={6}>
+              <Link href={"/articles/" + article.id}>
+                <ArticleCard
+                  title={article.title}
+                  fromToday={article.fromToday}
+                  userName={article.user.name}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
 
-export default Index
+export default Index;
